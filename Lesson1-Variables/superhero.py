3e6fbf27dec7534e28f16fcf3ab6d4fb93949528
Justin <juspua3@gmail.com>
2025-09-08 21:37:13 -0400

    Superhero exercise done

---
+++ Lesson1-Variables/superhero.py
@@ -0,0 +1,14 @@
+words = ["===", "CHARACTER PROFILE", "==="]
+header = " ".join(words)
+
+name = "Delta Six"
+age = "21"
+height = "5.9"
+superhero = True
+print(header)
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Height: {height} feet")
+print(f"Superhero: {superhero}")
+print("Main Power: Can change physiology to adapt to any battlefield.")
+
